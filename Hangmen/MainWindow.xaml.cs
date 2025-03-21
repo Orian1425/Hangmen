ea578ffa6ea37393b3913f25a5fed2bc2712bc39
Orian1245 <orianya130@gmail.com>
2025-03-21 11:04:21 +0200

    first version

--- Hangmen/MainWindow.xaml.cs
+++ Hangmen/MainWindow.xaml.cs
@@ -20,9 +20,71 @@ namespace Hangmen
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
+        private List<string> wordList = new List<string>()
+        {
+            "apple", "banana","grape fruit","watermelon","strawberry","mango","kiwi"
+        };
+        private string selectedWord;
+        private char letter;
+        private int letters = 0;
         public MainWindow()
         {
             InitializeComponent();
+
+            for(char ch = 'a'; ch <= 'z'; ch++)
+            {
+                Button btn = new Button
+                {
+                    
+                    Content = ch.ToString(), 
+                    FontSize = 30,
+                    Margin = new Thickness(2)
+                    
+                };
+                btn.Click += Btn_Click;
+                lettersGrid.Children.Add(btn);
+            }
+        }
+
+        private void New_Word(object sender,RoutedEventArgs e)
+        {
+            wordGrid.Children.Clear();
+         
+            int rndNum = rnd.Next(0,wordList.Count());
+            selectedWord = wordList[rndNum];
+            wordList.Remove(selectedWord);
+
+            for(int i = 0; i<selectedWord.Length; i++)
+            {
+                TextBox tb = new TextBox
+                {
+                    IsReadOnly = true,
+                    Text = selectedWord[i].ToString(),
+                    Foreground = Brushes.White,
+                    Margin = new Thickness(5),
+                    FontSize = 30,
+                };
+                if (selectedWord[i] == ' ')
+                {
+                    letter++;
+                    tb.BorderBrush = new SolidColorBrush(Colors.Transparent);
+                }
+                wordGrid.Children.Add(tb);
+            }
+            if(wordList.Count() == 0)
+            {
+                MessageBox.Show("no more words", "game ended", MessageBoxButton.OK,MessageBoxImage.None);
+                Close();
+            }
+            //
+        }
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.IsEnabled = false;
+
+            letter = char.Parse(btn.Content.ToString());
         }
     }
 }
