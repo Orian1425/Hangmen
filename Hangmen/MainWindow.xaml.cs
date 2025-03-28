f95364a0f99923ca1be34ba506f5fc0e9311844b
Orian1245 <orianya130@gmail.com>
2025-03-28 20:21:36 +0300

    datatbasde

--- Hangmen/MainWindow.xaml.cs
+++ Hangmen/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.OleDb;
+using DAL;
 
 namespace Hangmen
 {
@@ -20,9 +23,152 @@ namespace Hangmen
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
+        //private List<string> wordList = new List<string>();
+        private DataTable wordDataTable = new DataTable();
+        private string selectedWord;
+        private int letters = 0;
+        private int guessedWords = 0;
+        private int maxFail = 10;
+        private int fail = 10;
         public MainWindow()
         {
             InitializeComponent();
+
+            MakeLetters();
+            New_Word(null,null);
+            
+        }
+        private void MakeLetters()
+        {
+            this.lettersGrid.Children.Clear();
+            for (char ch = 'a'; ch <= 'z'; ch++)
+            {
+                Button btn = new Button
+                {
+
+                    Content = ch.ToString(),
+                    FontSize = 30,
+                    Margin = new Thickness(2)
+
+                };
+                btn.Click += Btn_Click;
+                this.lettersGrid.Children.Add(btn);
+            }
+        }
+
+        private void New_Word(object sender,RoutedEventArgs e)
+        {
+            this.fail = 10;
+            this.DisplayFailImage();
+            this.checkEnd();
+            this.wordGrid.Children.Clear();
+            this.MakeLetters();
+            this.wordLetters.Clear();
+            letters = 0;
+            int rndNum = this.rnd.Next(0,this.wordList.Count());
+            this.selectedWord = this.wordList[rndNum];
+            this.wordList.Remove(selectedWord);
+
+            for(int i = 0; i<this.selectedWord.Length; i++)
+            {
+                TextBox tb = new TextBox
+                {
+                    IsReadOnly = true,
+                    Name = (this.selectedWord[i] != ' ' ) ? Name = selectedWord[i].ToString() : "S" ,
+                    Margin = new Thickness(5),
+                    FontSize = 30,
+                    
+                };
+
+                if (selectedWord[i] == ' ')
+                {
+                    this.letters++;
+                    tb.BorderBrush = new SolidColorBrush(Colors.Transparent);
+                }
+                this.wordLetters.Add(selectedWord[i]);
+                this.wordGrid.Children.Add(tb);
+            }
+            
+            //
+        }
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.IsEnabled = false;
+
+            char letter = char.Parse(btn.Content.ToString());
+
+            if(selectedWord.Contains(letter))
+            {
+                letters++;
+                foreach(TextBox tb in this.wordGrid.Children)
+                {
+                    if (tb.Name.Equals(letter.ToString()))
+                    {
+                        tb.Text = letter.ToString();
+                    }
+                }
+                if(letters == wordLetters.Count())
+                {
+                    MessageBox.Show("you gussed the word! - "+this.selectedWord,"Good Job",MessageBoxButton.OK,MessageBoxImage.None);
+                    guessedWords++;
+                    this.guessedWordsCounter.Text = "Guessed Words: "+guessedWords;
+                    foreach (Button b in this.lettersGrid.Children)
+                    {
+                        b.IsEnabled = false;
+                    }
+                    this.checkEnd();
+                }
+            }
+            else
+            {
+                this.fail--;
+                this.DisplayFailImage();
+                if(this.fail == 0)
+                {
+                    MessageBox.Show("you failed!", "Lose",MessageBoxButton.OK,MessageBoxImage.None);
+                    foreach (Button b in this.lettersGrid.Children)
+                    {
+                        b.IsEnabled = false;
+                    }
+                    this.Show_Solution(null,null);
+                    this.checkEnd();
+                }
+            }
+         
+        }
+        private void Show_Solution(object sender, RoutedEventArgs e)
+        {
+            foreach (TextBox tb in this.wordGrid.Children)
+            {
+                tb.Text = tb.Name != "S" ? tb.Name.ToString() : "";
+            }
+            MessageBox.Show("Showed solution", "game ended", MessageBoxButton.OK, MessageBoxImage.None);
+            foreach (Button b in this.lettersGrid.Children)
+            {
+                b.IsEnabled = false;
+            }
+            this.checkEnd();
+
+        }
+        private void DisplayFailImage()
+        {
+            string picName = $"{maxFail-fail}.jpg";
+
+            var uri = new Uri("C://Users//Alex//Downloads//Hangmen//Hangmen//images//" + picName);
+            var bitmap = new BitmapImage(uri);
+
+            manImage.Source = bitmap;
+        }
+        private void checkEnd()
+        {
+            if (this.wordList.Count() == 0)
+            {
+                MessageBox.Show("no more words", "game ended", MessageBoxButton.OK, MessageBoxImage.None);
+                Close();
+            }
+        
         }
     }
 }
